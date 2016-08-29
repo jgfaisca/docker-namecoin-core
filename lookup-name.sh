7d4c33b3e2d5c83622b304e21e0e2a42db73ff4c
zekaf <jose.faisca@gmail.com>
2016-08-29 12:56:52 +0000

    Update

--- lookup-name.sh
+++ lookup-name.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #########################################################
-# 
-# Test Namecoin RPC connection on localhost
-# 
-# usage: 
-# ./test-rpc-connection <data_directory>
+#
+# Lookup Namecoin name using RPC connection on localhost
+#
+# usage:
+# ./lookup-name.sh <name>
 #
 # example:
-# ./test-rpc-connection /home/myuser/.namecoin
+# ./lookup-name.sh d/okturtles
 #
 # authors:
 # jose G. Faisca <jose.faisca@gmail.com>
@@ -17,22 +17,24 @@
 if [ $# -ne 1 ]; then
   echo
   echo "Invalid number of arguments."
-  echo "Usage: ./$(basename "$0") <data_directory>"
+  echo "Usage: ./$(basename "$0") <namecoin_name>"
   echo
   exit 1
 fi
 
-DATADIR=$1
+DATADIR=/data/namecoin
 CONF=namecoin.conf
 HOST="127.0.0.1"
+NAME="$1"
 
 RPC_USER=$(cat $DATADIR/$CONF | grep rpcuser | awk -F '[/=]' '{print $2}')
 RPC_PASS=$(cat $DATADIR/$CONF | grep rpcpassword | awk -F '[/=]' '{print $2}')
 RPC_PORT=$(cat $DATADIR/$CONF | grep rpcport | awk -F '[/=]' '{print $2}')
 
-JSON=$(curl -D -sS --user $RPC_USER:$RPC_PASS \
-        --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"getinfo","params":[]}' \
+# lookup name
+JSON=$(curl -D -sS --user ${RPC_USER}:${RPC_PASS} \
+        --data-binary '{"jsonrpc":"1.0","id":"curltext","method":"name_show","params":["'"${NAME}"'"]}' \
         -H 'content-type: text/plain;' \
-        http://$HOST:$RPC_PORT)
+        http://${HOST}:${RPC_PORT})
 
 echo $JSON | json_pp
